3a4ea9baddfc1192f1862dcd281794457981229c
Benjamin Rojas-Ritchie <ben@benarr.com>
2021-08-02 09:36:34 -0600

    previous page, contact success

--- src/js/script.js
+++ src/js/script.js
@@ -8,3 +8,14 @@ window.onscroll = function() {
         scrolltotop.style.bottom = "40px";
     }
 };
+
+function previousPage() {
+    var oldURL = document.referrer.split('/')[2].split(':')[0];
+    var newURL = window.location.hostname;
+    alert(oldURL);
+    alert(newURL);
+    if (oldURL == newURL)
+        window.history.go(-1);
+    else 
+        window.location.href = "https://www.sannicolastech.com/"
+}
